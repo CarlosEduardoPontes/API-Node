d73560f97a5b27710ed552a576430521d4c6132e
CarlosEduardoPontes <ceduardo.pontes@hotmail.com>
2022-05-06 23:38:54 -0300

    criação do arquivo rotas

---
+++ src/routes/routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',function(req,res,next){
+    res.send("hello world")
+})
+router.get('/home',function(req,res,next){
+    res.send("hello world")
+})
+router.get('/cadastro',function(req,res,next){
+    res.send("Cadastre-se")
+})
+router.get('/login',function(req,res,next){
+    res.send("Login")
+})
+
+
+
+
+
+module.exports = router
