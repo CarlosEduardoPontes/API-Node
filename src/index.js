98f902516774dc9e2b0bdbf98aa44917c5656bec
CarlosEduardoPontes <ceduardo.pontes@hotmail.com>
2022-05-06 23:39:47 -0300

    atualização do servidor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 const express = require('express')
 const cors = require('cors')
-
+const routes = require('./routes/routes')
 const app = express()
-
 
 app.use(cors())
 app.use(express.json())
-
+app.use(routes)
 
 
 const PORT = 3000 
-
 app.listen(PORT, ()=>{
-    console.log("Servidor rodando na porta 3000")
+    console.log("Servidor rodando na porta", PORT)
 })
